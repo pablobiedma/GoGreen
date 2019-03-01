b52985b36528cb5cb621abf1f519f7a09b160ccf
Flo <favandervlist@student.tudelft.nl>
2019-03-01 16:31:40 +0100

    Added loginscreen and menu of the GUI Client

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -6,9 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.net.URL;
-
 public class Main extends Application {
     // This will start a stage (program window)
     public static void main(String[] args) {
@@ -19,10 +16,10 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         // The FXML file (in which buttons/textviews/sizes etc. are declared) will be
         // read and set as scene (the stuff inside the window of the program)
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI.fxml"));
-        Scene scene = new Scene(root, 500, 400);
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Login.fxml"));
+        Scene scene = new Scene(root, 900, 700);
 
-        stage.setTitle("ECO Green Group 12");
+        stage.setTitle("ECO Green | Group 12");
         stage.setScene(scene);
         stage.show();
     }
