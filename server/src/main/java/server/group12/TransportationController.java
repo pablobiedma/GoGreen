f139cff1a70ccf60c6fa19fa3d3f138ca4c4e9c9
Ivan <I.Dimitrov@student.tudelft.nl>
2019-03-04 15:35:34 +0100

    Database support.

--- server/src/main/java/server/group12/TransportationController.java
+++ server/src/main/java/server/group12/TransportationController.java
@@ -1,5 +1,6 @@
 package server.group12;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +13,16 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 @RestController
 public class TransportationController {
+    @Autowired
+    private VehicleUsageRepository repository;
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(method = RequestMethod.GET, value = "/transport")
     public Transportation transportation(@RequestParam(value = "vehicle",
                                                        defaultValue = "Unknown")
                                           String vehicleType) {
-        return new Transportation(counter.incrementAndGet(), vehicleType);
+        repository.save(new VehicleEntry(counter.incrementAndGet(), vehicleType));
+        return new Transportation(counter.get(),vehicleType);
     }
 }
