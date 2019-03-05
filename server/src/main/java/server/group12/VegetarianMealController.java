ed4b337e2d614e528da4c3374f771953d9d2e914
Ivan Dimitrov <i.dimitrov@student.tudelft.nl>
2019-03-05 18:44:52 +0000

    Moved the new classes to the group12 package

---
+++ server/src/main/java/server/group12/VegetarianMealController.java
@@ -0,0 +1,25 @@
+package server.group12;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/vegetarianmeal")
+public class VegetarianMealController {
+    @Autowired
+    private VegetarianMealUsageRepository repository;
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value="/save", method = RequestMethod.POST)
+    public VegetarianMealEntry createVegetarianMeal(@Valid @RequestBody VegetarianMealEntry vegetarianMealEntry){ {
+        vegetarianMealEntry.setId(ObjectId.get());
+        repository.save(vegetarianMealEntry);
+        return vegetarianMealEntry;
+    }
+    }
+}
---
+++ server/src/main/java/server/group12/VegetarianMealUsageRepository.java
@@ -0,0 +1,10 @@
+package server.group12;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface VegetarianMealUsageRepository extends MongoRepository<VegetarianMealEntry, String> {
+    public VegetarianMealEntry getByUserId(long userId);
+    public List<VegetarianMealEntry> getByVegetarianMeal(String vegetarianMeal, int points, int co2);
+}
