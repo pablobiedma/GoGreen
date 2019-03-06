3de81df9030bc556ecd1c2820af9f0b30ded6cdd
Ivan <I.Dimitrov@student.tudelft.nl>
2019-03-06 22:05:34 +0100

    Minor change.

--- server/src/main/java/server/group12/VehicleUsageRepository.java
+++ server/src/main/java/server/group12/VehicleUsageRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface VehicleUsageRepository extends MongoRepository<VehicleEntry,String> {
-    public VehicleEntry getByUserId(long userId);
     public List<VehicleEntry> getByVehicleType(String vehicleType);
-    public VehicleEntry getById(ObjectId id);
+    public VehicleEntry getById(ObjectId id); //using ObjectId since it is unique
 }
