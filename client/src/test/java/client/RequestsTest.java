8573d33520e5baac6e0f7c1046f6d05e5e4e80a3
favandervlist <Hofkamplaan22!>
2019-03-06 13:19:10 +0100

    Fixed client tests

--- client/src/test/java/client/RequestsTest.java
+++ client/src/test/java/client/RequestsTest.java
@@ -1,25 +1,22 @@
-package client;
+package client.group12;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
 import java.io.IOException;
-
 import static org.junit.Assert.*;
 
 public class RequestsTest {
 
+    Requests request = new Requests();
+
     @Test
     public void carRequestTest() throws IOException {
-        assertTrue(Requests.getRequest("car").contains("At least use public transport!")); }
+        assertTrue(request.getRequest("vehicle=car", "transport").contains("At least use public transport!")); }
 
     @Test
     public void publicTransportRequestTest() throws IOException {
-        assertTrue(Requests.getRequest("public_transport").contains("Use a bike instead")); }
+        assertTrue(request.getRequest("vehicle=public_transport", "transport").contains("Use a bike instead")); }
 
     @Test
     public void bikeRequestTest() throws IOException {
-        assertTrue(Requests.getRequest("bike").contains("Good job!")); }
+        assertTrue(request.getRequest("vehicle=bike", "transport").contains("Good job!")); }
 }
