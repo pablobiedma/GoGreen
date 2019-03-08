c19b55e45d4e0321d8f7e23faf35575ecb166bd9
Flo <favandervlist@student.tudelft.nl>
2019-03-08 18:31:57 +0100

    Connected all the calculations to the GUI, in the commandline you can see that the program works and can calculate

--- client/src/main/java/client/group12/controllers/VegetarianMealController.java
+++ client/src/main/java/client/group12/controllers/VegetarianMealController.java
@@ -1,19 +1,16 @@
 package client.group12.controllers;
 
+import client.group12.vegetarianMeal.Calculations;
 import client.group12.vegetarianMeal.MealAPI;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -21,14 +18,17 @@ import java.util.ResourceBundle;
 public class VegetarianMealController implements Initializable {
 
     @FXML
-    private ListView<String> mealList;
-
-    @FXML
     private ChoiceBox<String> cb = new ChoiceBox();
 
     @FXML
-    public void calculate(MouseEvent event) throws Exception {
-    }
+    private ChoiceBox<String> cb1 = new ChoiceBox();
+
+    @FXML
+    private Slider slider = new Slider();
+
+    private String goodFoodName = "";
+    private String badFoodName = "";
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
@@ -43,9 +43,13 @@ public class VegetarianMealController implements Initializable {
         List<String> items = Arrays.asList(listItems.split(", "));
         ObservableList<String> list = FXCollections.observableArrayList(items);
         cb.getItems().addAll(list);
+        cb1.getItems().addAll(list);
+
+
     }
 
-    public void badFoodName() throws IOException {
+    @FXML
+    public void badFoodName() {
 
     }
 
@@ -56,6 +60,17 @@ public class VegetarianMealController implements Initializable {
 
     @FXML
     public void calculateCO2(MouseEvent event) throws Exception {
-
+        badFoodName = cb1.getValue();
+        goodFoodName = cb.getValue();
+        System.out.println(goodFoodName + "    and   " + badFoodName);
+        int servingSize = (int) slider.getValue();
+        System.out.println(servingSize);
+        Calculations calculation = new Calculations();
+        double CO2GoodFood = calculation.calculateCO2(goodFoodName, servingSize);
+        System.out.println(CO2GoodFood);
+        int reducedCO2 = calculation.reducedCO2(badFoodName, servingSize, CO2GoodFood);
+        int points = calculation.calculatePoints();
+        //SEND POST REQUEST TO UPDATE THE DATABASE
+        System.out.println("Reduced CO2: " + reducedCO2 + "   Amount of points earned: "+points);
     }
 }
--- client/src/main/java/client/group12/vegetarianMeal/Calculations.java
+++ client/src/main/java/client/group12/vegetarianMeal/Calculations.java
@@ -1,5 +1,6 @@
 package client.group12.vegetarianMeal;
 
+import java.io.IOException;
 import java.lang.Math;
 import java.util.List;
 
@@ -8,14 +9,14 @@ public class Calculations {
     private double calculatedCO2;
     private String food;
     private int co2;
-    private int calculatedPoints;
     private double servingSize;
     private int reducedCO2;
 
     //this is the first calculation that is done. the Chosen food is looked up in the mealList and than the private values are updated
     // and the CO2 emmision is calculated.
-    public double calculateCO2(String chosenFood, int chosenServingSize){
+    public double calculateCO2(String chosenFood, int chosenServingSize) throws IOException {
         MealAPI mealapi = new MealAPI();
+        mealapi.readAPI();
         List<Meal> mealList = mealapi.getMealList();
         for(int i = 0; i < mealList.size(); i++){
             if(chosenFood.equals(mealList.get(i).getFood())){
@@ -24,21 +25,21 @@ public class Calculations {
                 this.servingSize = mealList.get(i).getServingSize();
 
                 double co2PerGram = co2/servingSize;
-                this.calculatedCO2 = co2PerGram*chosenServingSize;
+                calculatedCO2 = co2PerGram*chosenServingSize;
             }
         }
-        return this.calculatedCO2;
+        return calculatedCO2;
+    }
+
+    //this is the second calculation that is done, people also choose a bad food what they normally eat, so the reduced CO2 can be calculated.
+    public int reducedCO2(String badFood, int chosenServingSize, double CO2GoodFood) throws IOException{
+        this.reducedCO2 = (int) (calculateCO2(badFood, chosenServingSize) - CO2GoodFood);
+        //System.out.println(reducedCO2);
+        return this.reducedCO2;
     }
 
     //this is the last calculation that is done, the points for the user are calculated.
     public int calculatePoints(){
-        return (int) Math.round(this.reducedCO2*5);
-    }
-
-    //this is the second calculation that is done, people also choose a bad food what they normally eat, so the reduced CO2 can be calculated.
-    public int reducedCO2(String badFood, int chosenServingSize){
-        double badFoodCO2 = calculateCO2(badFood, chosenServingSize);
-        this.reducedCO2 = (int) Math.round(badFoodCO2 - calculatedCO2);
-        return this.reducedCO2;
+        return (int) this.reducedCO2*5;
     }
 }
