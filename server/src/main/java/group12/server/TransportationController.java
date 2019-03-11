a88e0749bc801e14978dbb9d260a6081a31e5550
Ivan <I.Dimitrov@student.tudelft.nl>
2019-03-11 16:23:10 +0100

    Added RequestMapping annotation

--- server/src/main/java/group12/server/TransportationController.java
+++ server/src/main/java/group12/server/TransportationController.java
@@ -2,6 +2,7 @@ package group12.server;
 
 import group12.database.Database;
 import group12.database.VehicleEntry;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
