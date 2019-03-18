16ac90bf38024712c0da0d2dfc7b83537e1848a8
Flo <favandervlist@student.tudelft.nl>
2019-03-18 14:48:43 +0100

    test for vegetarianMealController

---
+++ server/src/test/java/groupxii/server/VegetarianMealControllerTest.java
@@ -0,0 +1,28 @@
+package groupxii.server;
+
+import groupxii.database.MealEntry;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class VegetarianMealControllerTest {
+
+    VegetarianMealController vegetarianMealController = new VegetarianMealController();
+
+    @Test
+    public void saveMealTest() throws IOException {
+        vegetarianMealController.getMealData();
+        MealEntry entry = vegetarianMealController.saveMealData("APPLES", 50, "HAZELNUTS", 50);
+        assertEquals(entry.getBadFoodName(), "HAZELNUTS");
+        assertEquals(entry.getGoodServingSize(), 50);
+    }
+
+    @Test
+    public void getNameList() throws IOException {
+        vegetarianMealController.getMealData();
+        String nameList = vegetarianMealController.getNameList();
+        assertTrue(nameList.contains("APPLES"));
+    }
+}
