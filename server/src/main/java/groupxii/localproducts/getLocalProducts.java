c0156ab744f853ce0dfbcf6c4b9e25f9ccf70e09
Flo <favandervlist@student.tudelft.nl>
2019-03-21 21:46:11 +0100

    Added the class that gets response from Google Maps API

---
+++ server/src/main/java/groupxii/localproducts/getLocalProducts.java
@@ -0,0 +1,43 @@
+package groupxii.localproducts;
+
+import com.google.maps.StaticMapsRequest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class getLocalProducts {
+
+    private URL url;
+
+    public String getLocalShopJson(){
+        String localShopDataJson = "";
+        try {
+            url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.00247,4.35814&radius=10000&rankby=prominence&keyword=supermarkt&key=AIzaSyAx468de0kVfaSUaksLjRhjPyXdTjnuYZQ");
+            localShopDataJson = readJson();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return localShopDataJson;
+    }
+
+    public String readJson() throws IOException {
+        String readLine;
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/json");
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuffer response = new StringBuffer();
+        while ((readLine = in.readLine()) != null) {
+            response.append(readLine);
+        }
+        in.close();
+        System.out.println(response.toString());
+        return response.toString();
+    }
+}
