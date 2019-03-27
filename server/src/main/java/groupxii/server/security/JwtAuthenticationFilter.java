a9a6535fd938d1eeebdfcf70a530e5c42ab7c244
Lazar Nenovski <L.Nenovski@student.tudelft.nl>
2019-03-27 16:39:35 +0100

    /login now responds only on POST and reads credentials from the json body

--- server/src/main/java/groupxii/server/security/JwtAuthenticationFilter.java
+++ server/src/main/java/groupxii/server/security/JwtAuthenticationFilter.java
@@ -3,8 +3,12 @@ package groupxii.server.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 //import org.springframework.security.core.GrantedAuthority;
@@ -16,7 +20,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.Date;
-//import java.util.stream.Collectors;
+import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -30,11 +35,36 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException { //LOOOONG
-	    //TODO read those from the request body
-        String username = request.getParameter("username"); //BAD
-        String password = request.getParameter("password"); //VERY VERY BAD
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password); // JAVA IS SHIT LANGUAGE
+                                                HttpServletResponse response) throws AuthenticationException {
+	String body = null;
+
+	//Is there a better way for this?
+	//TODO return BAD_REQUEST here
+	if (!("POST".equalsIgnoreCase(request.getMethod())))
+		throw new BadCredentialsException("Request must be POST");
+
+	try {
+		body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+	} catch (IOException e) {
+		//TODO return BAD REQUEST here
+		throw new BadCredentialsException("Could not read request body");
+	}
+
+	JSONObject json = null;
+
+	String username = null;
+	String password = null;
+
+	try {
+		json = new JSONObject(body);
+		username = json.getString("username");
+		password = json.getString("password");
+	} catch (JSONException e) {
+		//TODO return BAD REQUEST here?
+		throw new BadCredentialsException("Could not parse JSON");
+	}
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
 
         return authenticationManager.authenticate(authenticationToken);
     }
