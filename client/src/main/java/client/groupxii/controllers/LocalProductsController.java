293c93c987d5cbc3a71b143d844d48004c1d6efc
Flo <favandervlist@student.tudelft.nl>
2019-03-29 19:14:52 +0100

    adding a google map to show the user where the shops are

--- client/src/main/java/client/groupxii/controllers/LocalProductsController.java
+++ client/src/main/java/client/groupxii/controllers/LocalProductsController.java
@@ -8,10 +8,13 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 
@@ -25,6 +28,9 @@ public class LocalProductsController implements Initializable {
 
     @FXML
     private ListView<String> localShops = new ListView();
+
+    @FXML
+    private ImageView mapsImage;
 
     private List<String> listViewItems = new ArrayList<String>();
     private ObservableList<String> listViewObservable;
@@ -44,6 +50,7 @@ public class LocalProductsController implements Initializable {
         listViewObservable = FXCollections.observableArrayList(listViewItems);
         System.out.println(listItemsStr);
         localShops.getItems().addAll(listViewObservable);
+        mapsImage = new ImageView(new Image("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x250&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyBvn_zZpLGUjLJBxIUoGHgJjzo2VlZm3jg"));
     }
 
     @FXML
