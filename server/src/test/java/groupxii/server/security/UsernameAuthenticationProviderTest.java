0922d0399152a778999cc02214f2ec2991997848
Lazar Nenovski <L.Nenovski@student.tudelft.nl>
2019-03-30 14:00:40 +0100

    Added tests for UsernameAuthenticaionProvider

---
+++ server/src/test/java/groupxii/server/security/UsernameAuthenticationProviderTest.java
@@ -0,0 +1,61 @@
+package groupxii.server.security;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class UsernameAuthenticationProviderTest {
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void missingUsernameTest() {
+		thrown.expect(BadCredentialsException.class);
+		thrown.expectMessage("Missing username");
+		UsernameAuthenticationProvider uap = new UsernameAuthenticationProvider();
+		Authentication missingUsername = new UsernamePasswordAuthenticationToken(null, null);
+
+		uap.authenticate(missingUsername);
+	}
+
+	@Test
+	public void testSupportTrue() {
+		UsernameAuthenticationProvider uap = new UsernameAuthenticationProvider();
+		assertTrue(uap.supports(UsernamePasswordAuthenticationToken.class));
+	}
+
+	@Test
+	public void testSupportFalse() {
+		UsernameAuthenticationProvider uap = new UsernameAuthenticationProvider();
+		assertFalse(uap.supports(AnonymousAuthenticationToken.class));
+	}
+
+	// THE FOLLOWING TESTS ARE TEMP HACK
+	
+	@Test
+	public void testUsername0day() {
+		UsernameAuthenticationProvider uap = new UsernameAuthenticationProvider();
+		Authentication zeroDay = new UsernamePasswordAuthenticationToken("0day", null);
+		uap.authenticate(zeroDay);
+		// No exception thrown -> test pass
+	}
+
+	@Test
+	public void testUsername1day() {
+		thrown.expect(BadCredentialsException.class);
+		thrown.expectMessage("Username verification failed");
+		UsernameAuthenticationProvider uap = new UsernameAuthenticationProvider();
+		Authentication firstDay = new UsernamePasswordAuthenticationToken("1day", null);
+		uap.authenticate(firstDay);
+	}
+
+
+
+}
