913e285ab25486c33c1cf698f69ca9fdc8286d07
Ayush Patandin <S.A.S.patandin@student.tudelft.nl>
2019-04-09 20:10:12 +0200

    Added savePanelData test

--- server/src/test/java/groupxii/server/SolarPanelControllerTest.java
+++ server/src/test/java/groupxii/server/SolarPanelControllerTest.java
@@ -1,12 +1,16 @@
 package groupxii.server;
 
+import com.mongodb.DBObject;
 import groupxii.database.Database;
+import groupxii.database.PanelEntry;
 import groupxii.server.controllers.SolarPanelController;
 import groupxii.solarpanels.CalculatedPanel;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -37,6 +41,23 @@ public class SolarPanelControllerTest {
         assertFalse(namelist.isEmpty());
     }
 
+    @Test
+    public void savePanelData() {
+        PanelEntry panelEntry  = new PanelEntry("Monocrystalline", 34,45);
+        Principal principal = new Principal() {
+            @Override
+            public String getName() {
+                return "user1";
+            }
+        };
+        String username = principal.getName();
+        Database.instance.addUsedPanel(username, panelEntry);
+        assertTrue(solarPanelController.getNameList().equals(Database.instance.getPanelListPanelNames()));
+    }
 
+    @Test
+    public void getUsedPanelList() {
+
+    }
 
 }
