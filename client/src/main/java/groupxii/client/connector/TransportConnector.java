cc69f7ecb2bc18e4e3adf965e865de6245b2e7cb
Pablo Biedma <P.BiedmaNunez@student.tudelft.nl>
2019-04-11 13:33:15 +0200

    introducing connector

---
+++ client/src/main/java/groupxii/client/connector/TransportConnector.java
@@ -0,0 +1,23 @@
+package groupxii.client.connector;
+
+public class TransportConnector {
+    public static String retrieveTransportList(){
+        String resource = "/vehicleNameList";
+        return Connector.getRequest(resource);
+    }
+    public static String calculateCO2Reduction(String goodTransportName, String badTransportName , int goodConsumption , int badConsumption) {
+        String resource = "/calculateTransport"
+                + "?" + goodTransportName
+                + "&" + badTransportName
+                + "&" + goodConsumption
+                + "&" + badConsumption;
+
+        return Connector.getRequest(resource);
+    }
+    public static String retrieveUsedTransportList() {
+        String resource = "/usedTransportList";
+        return Connector.getRequest(resource);
+    }
+
+
+}
