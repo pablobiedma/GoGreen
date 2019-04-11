1e3290eebf7df631eb8147506519557b9e6baa60
Pablo Biedma <P.BiedmaNunez@student.tudelft.nl>
2019-04-11 13:37:41 +0200

    reducedco2 class

--- client/src/main/java/groupxii/client/transportation/ReducedCO2.java
+++ client/src/main/java/groupxii/client/transportation/ReducedCO2.java
@@ -1,4 +1,11 @@
 package groupxii.client.transportation;
 
+import groupxii.client.connector.TransportConnector;
+
 public class ReducedCO2 {
+
+    public static String getReducedCO2(String goodTransportName, String badTransportName , int goodConsumption , int badConsumption, int SafeVehicle){
+        String result = TransportConnector.calculateCO2Reduction(goodTransportName, badTransportName , goodConsumption ,badConsumption);
+        return result;
+    }
 }
