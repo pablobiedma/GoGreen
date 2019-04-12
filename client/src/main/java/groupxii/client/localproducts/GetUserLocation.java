f4fc8dc4c1ae60d67110af47bd6eaf3f944adcee
floerer <flovlist1@gmail.com>
2019-04-12 16:06:08 +0200

    deleting unwanted files and fixing errors

--- client/src/main/java/groupxii/client/localproducts/GetUserLocation.java
+++ client/src/main/java/groupxii/client/localproducts/GetUserLocation.java
@@ -18,11 +18,16 @@ public class GetUserLocation {
      * @throws IOException if it is not able to connect to the api.
      * @throws GeoIp2Exception if ip cannot be turned into a location.
      */
-    public static String getUserLocation() throws IOException, GeoIp2Exception {
+    public static String getUserLocation() {
         String ipAddress = Connector.instance.getRequest("http://eth0.me/");
         ObjectMapper objectMapper = new ObjectMapper();
         String url = "http://api.ipstack.com/" + ipAddress + "?access_key=f8449c29422a48b1dd367afadaa10714";
-        JsonNode rootNode = objectMapper.readTree(new URL("http://api.ipstack.com/" + ipAddress + "?access_key=f8449c29422a48b1dd367afadaa10714"));
+        JsonNode rootNode = null;
+        try {
+            rootNode = objectMapper.readTree(new URL("http://api.ipstack.com/" + ipAddress + "?access_key=f8449c29422a48b1dd367afadaa10714"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         String longitude = rootNode.get("longitude").asText();
         String latitude = rootNode.get("latitude").asText();
 
