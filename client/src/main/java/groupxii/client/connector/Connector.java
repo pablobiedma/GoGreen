f210279539c807afcd43983af8fef15751228c87
Lazar Nenovski <L.Nenovski@student.tudelft.nl>
2019-04-13 10:19:36 +0200

    Refactor Connector

--- client/src/main/java/groupxii/client/connector/Connector.java
+++ client/src/main/java/groupxii/client/connector/Connector.java
@@ -14,71 +14,60 @@ import java.net.URL;
 public class Connector {
     public static Connector instance = new Connector();
 
+    private static String request(String reqType, String resource, String body) {
+        HttpURLConnection connection;
+        try {
+            URL url = new URL(Target.getHost() + resource);
+            connection = (HttpURLConnection)url.openConnection();
+
+            if (reqType.equals("POST")) {
+                connection.setDoOutput(true);
+            }
+            connection.setRequestMethod(reqType);
+
+            if (reqType.equals("POST")) {
+                BufferedWriter requestBodyWriter = new BufferedWriter(
+                        new OutputStreamWriter(
+                            connection.getOutputStream()));
+                requestBodyWriter.write(body);
+                requestBodyWriter.close();
+            }
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String responseLine;
+            while ((responseLine = in.readLine()) != null) {
+                response.append(responseLine);
+			}
+            in.close();
+
+            // Temporary hack
+            // Since login is likely the only resource whose response
+            // will have Authorization filed...
+            if (resource.equals("/login")) {
+                return connection.getHeaderField("Authorization");
+            }
+
+            return response.toString();
+        } catch (Exception e) {
+            System.err.println("An connection error has occured");
+        }
+
+        return null;
+    }
+
 
     public static String getRequest(String resource) {
-    	HttpURLConnection connection;
-		try {
-			URL url = new URL(Target.getHost() + resource);
-			connection = (HttpURLConnection)url.openConnection();
+        return request("GET", resource, "");
+    }
 
-			connection.setRequestMethod("GET");
+    public static String postRequest(String resource) {
+        return request("POST", resource, "");
+    }
 
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(
-						connection.getInputStream()));
-			StringBuilder response = new StringBuilder();
-			String responseLine;
-			while ((responseLine = in.readLine()) != null)
-				response.append(responseLine);
-			in.close();
-
-			return response.toString();
-		} catch (Exception e) {
-			System.err.println("An connection error has occured");
-		}
-
-		return null;
-	}
-
-	public static String postRequest(String resource) {
-		return postRequest(resource, "");
-	}
-
-	public static String postRequest(String resource, String body) {
-		HttpURLConnection connection;
-		try {
-			URL url = new URL(Target.getHost() + resource);
-			connection = (HttpURLConnection)url.openConnection();
-
-			connection.setDoOutput(true);
-			connection.setRequestMethod("POST");
-
-			BufferedWriter requestBodyWriter = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-			requestBodyWriter.write(body);
-			requestBodyWriter.close();
-
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(
-							connection.getInputStream()));
-			StringBuilder response = new StringBuilder();
-			String responseLine;
-			while ((responseLine = in.readLine()) != null)
-				response.append(responseLine);
-			in.close();
-
-			// Temporary hack
-			// Since login is likely the only resource whose response
-			// will have Authorization filed...
-			String authorization = connection.getHeaderField("Authorization");
-			if (resource.equals("/login")) {
-				return authorization;
-			}
-
-			return response.toString();
-		} catch (Exception e) {
-			System.err.println("An connection error has occured");
-		}
-
-		return null;
-	}
+    public static String postRequest(String resource, String body) {
+        return request("POST", resource, body);
+    }
 }
