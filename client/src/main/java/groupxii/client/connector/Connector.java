5764ab532fd0b5a0b73bbd8c4565ffa95eb35c8c
Lazar Nenovski <L.Nenovski@student.tudelft.nl>
2019-04-13 20:51:02 +0200

    Split the try/catch block of the Connector

--- client/src/main/java/groupxii/client/connector/Connector.java
+++ client/src/main/java/groupxii/client/connector/Connector.java
@@ -2,9 +2,12 @@ package groupxii.client.connector;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 /**
@@ -15,47 +18,66 @@ public class Connector {
     public static Connector instance = new Connector();
 
     private static String request(String reqType, String resource, String body) {
+        //TODO append token as Authorization: Bearer <token>
         HttpURLConnection connection;
         try {
             URL url = new URL(Target.getHost() + resource);
             connection = (HttpURLConnection)url.openConnection();
-
             if (reqType.equals("POST")) {
                 connection.setDoOutput(true);
             }
-            connection.setRequestMethod(reqType);
+        } catch (MalformedURLException e) {
+            //TODO
+            return null;
+        } catch (IOException e) {
+            //TODO
+            return null;
+        }
 
-            if (reqType.equals("POST")) {
+
+        try {
+            connection.setRequestMethod(reqType);
+        } catch (ProtocolException e) {
+             //TODO
+             return null;
+        }
+
+        if (reqType.equals("POST")) {
+            try {
                 BufferedWriter requestBodyWriter = new BufferedWriter(
                         new OutputStreamWriter(
                             connection.getOutputStream()));
                 requestBodyWriter.write(body);
                 requestBodyWriter.close();
+            } catch (IOException e) {
+                //TODO
             }
+        }
 
+        String response = null;
+        try {
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(
                             connection.getInputStream()));
-            StringBuilder response = new StringBuilder();
+            StringBuilder responseBuilder = new StringBuilder();
             String responseLine;
             while ((responseLine = in.readLine()) != null) {
-                response.append(responseLine);
+                responseBuilder.append(responseLine);
             }
             in.close();
-
-            // Temporary hack
-            // Since login is likely the only resource whose response
-            // will have Authorization filed...
-            if (resource.equals("/login")) {
-                return connection.getHeaderField("Authorization");
-            }
-
-            return response.toString();
-        } catch (Exception e) {
-            System.err.println("An connection error has occured");
+            response = responseBuilder.toString();
+        } catch (IOException e) {
+            //TODO
         }
 
-        return null;
+        // Temporary hack
+        // Since login is likely the only resource whose response
+        // will have Authorization filed...
+        if (resource.equals("/login")) {
+            return connection.getHeaderField("Authorization");
+        }
+
+        return response;
     }
 
 
