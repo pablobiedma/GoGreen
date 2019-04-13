df3f13f20ed55326d190ab4235da284347af79d2
Lazar Nenovski <L.Nenovski@student.tudelft.nl>
2019-04-13 08:14:04 +0200

    Introduced TokenManager class

---
+++ client/src/main/java/groupxii/client/TokenManager.java
@@ -0,0 +1,51 @@
+package groupxii.client;
+
+/**
+ * Holds the verification token and periodically updates it from the server.
+ */
+
+public class TokenManager implements Runnable {
+	/**
+	 * Holds the JSON request to be posted on login.
+	 */
+	private String loginReqBody;
+
+	/**
+	 * Holds the JWT from the server.
+	 */
+	private String jwt;
+
+	/**
+	 * The daemon timeout should roughly match the validty of the token.
+	 * TODO read the validity off the token.
+	 */
+	public final static int timeout = 86390000;
+
+	public TokenManager(String username, String password) {
+
+		jwt = null;
+
+		loginReqBody = "{"
+			+ "\"username\":"
+			+ "\"" + username + "\", "
+			+"\"password\":"
+			+ "\"" + password + "\"}";
+		//TODO hash+salt the password
+	}
+
+	/**
+	 * Periodically requests a new token from the server.
+	 * Should be a daemon thread.
+	 */
+	public void run() {
+		while (true) {
+			refreshToken();
+		}
+	}
+
+	public void refreshToken() {
+		//TODO
+		//Connector.postLogin(reqBody) or something
+		//Store that response in jwt
+	}
+}
---
+++ client/src/test/java/groupxii/client/TokenManagerTest.java
@@ -0,0 +1,10 @@
+package groupxii.client;
+
+public class TokenManagerTest {
+	//TokenManager is ment to be a "blackbox" for security reasons
+	//TODO boiler plate? dunno
+
+	public void constructorTest() {
+		TokenManager tm = new TokenManager("usr", "password");
+	}
+}
