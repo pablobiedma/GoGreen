99855708fd3380fb9752d343a139d145971d6e6b
floerer <flovlist1@gmail.com>
2019-04-13 20:41:35 +0200

    gets userinput from the loginscreen

--- client/src/main/java/groupxii/client/controllers/LoginController.java
+++ client/src/main/java/groupxii/client/controllers/LoginController.java
@@ -3,22 +3,37 @@ package groupxii.client.controllers;
 import groupxii.client.Main;
 import groupxii.client.TokenManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 public class LoginController {
+
+    @FXML
+    private TextField txtUsername = new TextField();
+
+    @FXML
+    private PasswordField txtPassword = new PasswordField();
+
+    @FXML
+    private Text errorMessage = new Text();
+
     @FXML
     public void loginButton(MouseEvent event) throws Exception {
-		String username = "0day";
-		String password = "pass";
-		//TODO how do you read those?
+		if(txtUsername.getText().isEmpty() || txtPassword.getText().isEmpty()){
+		    errorMessage.setText("Please enter a username and/or password.");
+        } else{
+            String username = txtUsername.getText();
+            String password = txtPassword.getText();
+            // Initializa TokenManager
+            TokenManager.instance = new TokenManager(username, password);
 
-		// Initializa TokenManager
-		TokenManager.instance = new TokenManager(username, password);
+            //TODO error checks
 
-		//TODO error checks
-
-        Main main = new Main();
-        main.changeScene("Menu.fxml", event);
+            Main main = new Main();
+            main.changeScene("Menu.fxml", event);
+        }
     }
 
     @FXML
@@ -26,3 +41,4 @@ public class LoginController {
         System.out.println("Signup");
     }
 }
+
