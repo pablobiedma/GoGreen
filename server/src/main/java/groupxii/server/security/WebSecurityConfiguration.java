eb0d2d3d09ec1d5083dbeb9719fb02a0e6c1a3e4
Lazar Nenovski <L.Nenovski@student.tudelft.nl>
2019-04-14 14:47:06 +0200

    Added Proper configuration to the restricted endpoints

--- server/src/main/java/groupxii/server/security/WebSecurityConfiguration.java
+++ server/src/main/java/groupxii/server/security/WebSecurityConfiguration.java
@@ -57,9 +57,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic().disable()
                 .addFilter(new JwtGeneratingFilter(credentialsAuthentiactionManager()))
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/calculateMeal").permitAll();
+                	.antMatchers("/login").permitAll()
+			.and()
+		.authorizeRequests()
+			.antMatchers("/register").permitAll()
+			.and()
+		.authorizeRequests()
+			.anyRequest().authenticated();
     }
 
     @Bean
